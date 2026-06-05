547d38fcb5b40f76ded1111720845464cfaeb4eb
Masataro.Asai@ibm.com;3J6077897;MASATARO ASAI <masataro@login3.bluevela.rmf.ibm.com>
2026-06-05 13:12:37 +0000

    train_all.sh / eval_all.sh

---
+++ train_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Prohibit each job from accessing the huggingface hub to avoid the rate limitter.
+export HF_HUB_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
+
+parallel msub -p $(date +%Y%m%d%H%M)-kvpacket-train -c 4 -t 12h -m 256g -g 1 python run_train_filler.py \
+         ::: $(find packet_wrapper_config -name "*.json" | grep -v _default)
+
